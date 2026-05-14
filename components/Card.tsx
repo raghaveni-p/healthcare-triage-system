406943649e8b7e4c068349a9853630a4f5c5584b
raghaveni-p <praghareddy11@gmail.com>
2026-05-14 22:49:14 +0530

    Create Card component

---
+++ components/Card.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
+  children: React.ReactNode;
+}
+
+export default function Card({ children, className = '', ...props }: CardProps) {
+  return (
+    <div
+      className={`bg-white rounded-lg shadow-md p-6 ${className}`}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
